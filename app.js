f5ada4b8b29b725249a11a77035ef5bb466ade20
arya <raiabhishek04@gmail.com>
2016-06-09 18:43:44 +0530

    Add Image Galley which shows if product has images

--- app.js
+++ app.js
@@ -9,6 +9,16 @@
 	var gems = [
 	{
 		name: 'Doremon',
+		images: [
+		{
+			full: 'dore.jpg',
+			thumb: 'dore_small.jpg',
+		}
+		{
+			full: 'dore1.jpg',
+			thumb: 'dore1_small.jpg',
+		}
+		]
 		price: 2.95,
 		description: '....',
 		canPurchase: false,
@@ -16,6 +26,20 @@
 	},
 	{
 		name: 'Pikachu',
+		images: [
+		{
+			full: 'gore.jpg',
+			thumb: 'gore_small.jpg',
+		}
+		{
+			full: 'gore1.jpg',
+			thumb: 'gore1_small.jpg',
+		}
+		{
+			full: 'gore2.jpg',
+			thumb: 'gore2_small.jpg',
+		}
+		]
 		price: 50,
 		description: '......',
 		canPurchase: true,
